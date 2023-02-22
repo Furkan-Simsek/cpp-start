fb6d395350855e05987530ad949d6afb51e69a5f
Furkan Şimşek <tcfurkansimsek+github.com>
2023-02-22 17:34:51 +0300

    gui

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,9 +1,7 @@
 // wxWidgets "Hello world" Program
 // For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
-#ifndef WX_PRECOMP
-    #include <wx/wx.h>
-#endif
+#include <wx/wx.h>
+
 class MyApp: public wxApp
 {
 public:
